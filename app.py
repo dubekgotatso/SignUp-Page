55ee16ab74f769b7ffdfe6be88f36ab22cdf7c6c
kgotatsosurprise303@gmail.com <kgotatsosurprise303@gmail.com>
2024-04-18 16:40:00 +0200

    fries

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for,session
 from flask_pymongo import PyMongo
 from bson.objectid import *
+
 
 
 
@@ -41,17 +42,63 @@ def login():
         username = request.form['username']
         password = request.form['password']
 
-        # Check if user exists
+       
         existing_user = db.signup.find_one({'username': username, 'password': password})
         if existing_user:
             # Redirect to homepage or some other route
-            return redirect(url_for('home'))
+            return render_template("landing.html")
         else:
             # User not found, display error message
             return 'Invalid username or password'
 
     # Render the login form template
     return render_template('Login-Page.html')
+
+# Booking
+@app.route("/Booking", methods=["GET", "POST"])
+def view_bookings():
+    user_id = session.get('user_id')
+    if user_id:
+        bookings = mongo.db.booking.find({'user_id': ObjectId(user_id)})
+        if bookings.count()== 0:
+           return render_template('bookings.html', bookings=bookings)
+    return render_template("bookings.html") 
+
+@app.route("/AddBooking", methods=["GET", "POST"])
+def addbooking():
+  if request.method == 'POST':
+      category = request.form["category"]
+      time = request.form["time"]
+      date = request.form["date"]
+      price = request.form["price"]
+      
+      booking = {"category":category,"time":time,"date":date,"price":price}
+      db.booking.insert_one(booking)
+      if ('form submission success'):
+                     return render_template("bookings.html")
+      else:
+
+                  if ('form submission failed'):
+                   return 'form unsuccessful'
+               
+  return render_template("Addbooking.html")
+
+
+
+@app.route("/")
+def landingPage():
+    return render_template("testing.html")
+
+@app.route("/home")
+def home():
+    return render_template("landing.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)

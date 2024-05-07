8aa01b9d9eb4c485a0bfe215527bb61aade0041f
kgotatsosurprise303@gmail.com <kgotatsosurprise303@gmail.com>
2024-05-07 11:51:49 +0200

    07 May 2024

--- app.py
+++ app.py
@@ -105,10 +105,108 @@ def about():
     return render_template("about.html")
 
 
+# Add FadeWaveCut
+@app.route('/AddFadeWaveCut', methods=["POST", "GET"])
+def AddFadeWaveCut():
+    if request.method == 'POST':
+        name = request.form['name']
+        price = request.form['price']
+        
+        FadeWaveCut = { 'name': name, 'price': price}
+
+        db.FadeWaveCut.insert_one(FadeWaveCut)
+        if ('form submission success'):
+                     return redirect (url_for('getCut'))
+        else:
+
+                  if ('form submission failed'):
+                   return 'form unsuccessful'
+        
+    return render_template("AddFadeWaveCut.html")
+
+#Display FadeWaveCut
+@app.route("/FadeWaveCut", methods=["POST", "GET"] )
+def getCut():
+     if request.method == 'GET':
+          cut = []
+
+          for i in db.FadeWaveCut.find():
+            cut.append(i)
+            
+             
+     
+     return render_template("FadeWaveCuts.html" , x=cut )
+
+
+# Add DoubleDyeFade
+
+@app.route('/DoubleDyeFade', methods=["POST", "GET"])
+def AddDoubleDyeFade():
+    if request.method == 'POST':
+        name = request.form['name']
+        price = request.form['price']
+        
+        DoubleDyeFade= { 'name': name, 'price': price}
+
+        db.DoubleDyeFade.insert_one(DoubleDyeFade)
+        if ('form submission success'):
+                     return redirect (url_for('getDouble'))
+        else:
+
+                  if ('form submission failed'):
+                   return 'form unsuccessful'
+        
+    return render_template("AddDoubleDyeFade.html")
+
+
+ #Display DoubleDyeFade
+@app.route("/DoubleDyeFade", methods=["POST", "GET"] )
+def getDouble():
+     if request.method == 'GET':
+          Double = []
+
+          for i in db.DoubleDyeFade.find():
+            Double.append(i)
+            
+             
+     
+     return render_template("DoubleDyeFade.html" , x=Double )
 
 
 
+# Add DoubleDyeFade
 
+@app.route('/TopDyeHairCut', methods=["POST", "GET"])
+def AddTopDyeHairCut():
+    if request.method == 'POST':
+        name = request.form['name']
+        price = request.form['price']
+        
+        TopDyeHairCut = { 'name': name, 'price': price}
+
+        db.TopDyeHairCut.insert_one(TopDyeHairCut)
+        if ('form submission success'):
+                     return redirect (url_for('getDouble'))
+        else:
+
+                  if ('form submission failed'):
+                   return 'form unsuccessful'
+        
+    return render_template("AddTopDyeHairCut.html")
+
+
+ #Display TopDyeHairCut
+@app.route("/TopDyeHairFade", methods=["POST", "GET"] )
+def getTopDye():
+     if request.method == 'GET':
+          TopDye = []
+
+          for i in db.TopDyeHairCut.find():
+            TopDye.append(i)
+            
+             
+     
+     return render_template("TopDyeHairCut.html" , x=TopDye )
 
 if __name__ == "__main__":
     app.run(debug=True)

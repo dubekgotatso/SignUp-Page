9a9732ac7fdcb8a47408fd9e83fb4da2d1d9a749
kgotatsosurprise303@gmail.com <kgotatsosurprise303@gmail.com>
2024-09-02 11:11:25 +0200

    first commit

--- app/config.py
+++ app/config.py
@@ -1,3 +1,3 @@
 class Config:
-    MONGO_URI = "mongodb+srv://kgotatsosurprise303:AWPtDOFTi8ixjvYf@cluster0.8wfjsky.mongodb.net/MiniProject"
+    MONGO_URI = "mongodb+srv://kgotatsosurprise303:AWPtDOFTi8ixjvYf@cluster0.8wfjsky.mongodb.net/MiniProject22"
     SECRET_KEY = "POIUYTREWQ1234567890-POIUYTREWQ"
